c2272f2e7d7f93b0b379e368785fbf82c3b5eb43
cassiano <cassianothiago@rocketmail.com>
2023-07-10 21:15:34 -0400

    Update Pagamento.py

--- Pagamento.py
+++ Pagamento.py
@@ -12,56 +12,48 @@ def pagamento():
             if cliente=='0':
                 list_cliente.pop(-1)
                 break
-            try:
-                cont=0
-                while True:
+            while True:
+                    cont=0
                     cont=cont+1
                     conta=float(input('Valor da {}ª conta do cliente {} ou zero para encerrar = '.format(cont,cliente)))
-                    list_conta.append(conta)
+                    list_atraso.append(atraso)
                     if conta==0:
-                        list_conta.pop(-1)
-                        break
-                    try:
-                        atraso=int(input('A {}ª conta do cliente {} está atrasada?. Quantos dias?. (Sem atraso digite zero) =  '.format(cont,cliente)))
-                        list_atraso.append(atraso)
-                        if atraso>0:
-                            try:
-                                multa=float(input('Valor da multa = '))
-                                juros=float(input('Valor dos juros = '))
-                                pagar=conta+(conta*(multa/100))+(conta*(juros/100)*atraso)
-                                list_pagar.append(pagar)
-                            except ValueError:
-                                print('Multa ou juros invalido')
-                            except:
-                                print('Erro')
-                        elif atraso==0:
-                            list_pagar.append(conta)
+                        list_conta
+                        break         
+                    atraso=int(input('A {}ª conta do cliente {} está atrasada?. Quantos dias?. (Sem atraso digite zero) =  '.format(cont,cliente)))
+                    if atraso>0:
+                        try:
+                            multa=float(input('Valor da multa = '))
+                            juros=float(input('Valor dos juros = '))
+                            pagar=conta+(conta*(multa/100))+(conta*(juros/100)*atraso)
+                            list_pagar.append(pagar)
+                        except ValueError:
+                            print('Multa ou juros invalido')
+                        except:
+                            print('Erro')       
+                    elif atraso==0:
+                        list_pagar.append(conta)
                         valor=list_pagar(sum)
                         print(valor)
-                        try:
-                            dinheiro=float('Valor entregue pelo cliente = ')
-                        except ValueError:
-                            print('Valor entregue pelo clinte inválido')
-                        except:
-                            print('Erro')
-                        else:
-                            troco=dinheiro-valor
-                            while troco==0:
-                                print(troco)
-                                dinheiro=float('Valor entregue pelo cliente ou zero para encerrar = ')
-                    except ValueError:
-                        print('Atraso em dias invalido')
-                    except:
-                        print('Erro')
-            except ValueError:
-                print('Conta digitada invalida!!. Digite novamente!!')
-            except:
-                print('Erro')
+                        break 
     except ValueError:
         print('Nome do cliente inválido!!. Digite novamente!!')
     except:
         print('Erro')
+        
+  
+        try:
+                    dinheiro=float('Valor entregue pelo cliente = ')
+        except ValueError:
+                    print('Valor entregue pelo clinte inválido')
+        except:
+                    print('Erro')
+        else:
+                    troco=dinheiro-valor
+                    while troco==0:
+                        print(troco)
+                        dinheiro=float('Valor entregue pelo cliente ou zero para encerrar = ')      
+    
     print(list_cliente)
     print(list_conta)
 pagamento()
-print(list)
