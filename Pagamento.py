edcc40449ba14821bcc35b616fa3e6ca1bdbc90a
cassiano <cassianothiago@rocketmail.com>
2023-07-15 08:31:33 -0400

    Update Pagamento.py

--- Pagamento.py
+++ Pagamento.py
@@ -109,19 +109,19 @@ def pagamento():
                 list_valor.clear()
         system('pause')
         system('cls')                
-        while True:            
-                    imprimir=(input('Digite I para imprimir a fita de caixa ou o nome do cliente para imprimir específico e zero para fechar pagamento:  ')) 
-                    if imprimir=='0':
-                        break
-                    if imprimir =='I' or imprimir=='i':
-                        print(list_cliente)
-                        print(list_atraso)
-                        print(list_valor_final)
-                    else:
-                        for i in len(imprimir) in list_cliente:
-                            print(list_cliente[i])
-                            print(list_atraso[i])
-                            print(list_valor_final[i])
+    while True:            
+                imprimir=(input('Digite I para imprimir a fita de caixa ou o nome do cliente para imprimir específico e zero para fechar pagamento:  ')) 
+                if imprimir=='0':
+                    break
+                if imprimir =='I' or imprimir=='i':
+                    print(list_cliente)
+                    print(list_atraso)
+                    print(list_valor_final)
+                else:
+                    elemento=len(list_cliente)
+                    print(list_cliente[len(elemento)])
+                    print(list_atraso[i])
+                    print(list_valor_final[i])
                     
                 
               
