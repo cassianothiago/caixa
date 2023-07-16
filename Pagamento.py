441ecee02ae0999cb16bc5ea43f6e4fc5e8e024a
cassiano <cassianothiago@rocketmail.com>
2023-07-16 10:43:26 -0400

    Update Pagamento.py

--- Pagamento.py
+++ Pagamento.py
@@ -4,9 +4,10 @@ def pagamento():
     print('Bem vindo ao Pagamento')
     print('-----')
     
-    cont=0
+
     multa=False
     juros=False
+    atraso1=False
     list_cliente=[]
     list_conta=[]
     list_atraso=[]
@@ -25,58 +26,57 @@ def pagamento():
             list_cliente.append(cliente)
             while True:
                 try:    
-                    cont=cont+1
                     conta=float(input('Digite o valor da conta do cliente {} ou zero para encerrar: '.format(cliente)))
+                    atraso1=False
                 except ValueError:
                     print('Valor da conta invalido')
-                    cont=0
                 except:
                     print('Erro conta')
-                    cont=0
-                else:
-                    if conta==0:
-                        cont=0
-                        break
+                else:  
                     list_conta.append(conta)
-                    while True:
-                        try:
-                            atraso=int(input('Digite quantos dias a conta do cliente {} está atrasada '.format(cliente)))
-                            atraso1=True
-                        except ValueError:
-                            print('Dias de atraso invalido')
-                            atraso1=False
-                        except:
-                            print('Erro atrasado')
-                            atraso1=False
-                        finally:
-                            list_atraso.append(atraso)
-                            if atraso>0 and atraso1==True:
-                                while multa==False and juros==False:
-                                    try:
-                                        multa=float(input('Valor da multa = '))
-                                        juros=float(input('Valor dos juros = '))
-                                        multa==True
-                                        juros==True
-                                    except ValueError:
-                                        print('Multa e juros invalidos')
-                                        multa==False
-                                        juros==False
-                                    except:
-                                        print('Erro multa juros')
-                                        multa==False
-                                        juros==False
-                                    finally:
-                                        valor=conta+(conta*(multa/100)+conta*(atraso*(juros/100)))
-                                        list_valor.append(valor)
-                                        multa==True
-                                        juros==True
-                                        break
-                            elif atraso==0 and atraso1==True:
-                                multa==True
-                                juros==True
-                                list_valor.append(conta)
-                                break                                                                
-                        break        
+                    if conta!=0:
+                        while atraso1==False:
+                            try:
+                                atraso1=True
+                                atraso=int(input('Digite quantos dias a conta do cliente {} está atrasada '.format(cliente)))
+                            except ValueError:
+                                print('Dias de atraso invalido')
+                                atraso1=False
+                            except:
+                                print('Erro atrasado')
+                                atraso1=False
+                                if atraso>0 and atraso1==True:
+                                    list_atraso.append(atraso)
+                                    atraso1=True
+                                    while multa==False and juros==False:
+                                        try:
+                                            atraso1=True
+                                            multa=float(input('Valor da multa = '))
+                                            juros=float(input('Valor dos juros = '))
+                                            multa=True
+                                            juros=True
+                                        except ValueError:
+                                            print('Multa e juros invalidos')
+                                            multa=False
+                                            juros=False
+                                        except:
+                                            print('Erro multa juros')
+                                            multa=False
+                                            juros=False
+                                        else:
+                                            atraso1=True
+                                            valor=conta+(conta*(multa/100)+conta*(atraso*(juros/100)))
+                                            list_valor.append(valor)
+                                            multa=True
+                                            juros=True
+                                elif atraso==0:
+                                    list_atraso.append(atraso)
+                                    atraso1=True
+                                    multa=True
+                                    juros=True
+                                    list_valor.append(conta)                                                                
+                    if conta==0:
+                        break          
             try:
                 valor_final=sum(list_valor)
                 list_valor_final.append(valor_final)
@@ -113,7 +113,6 @@ def pagamento():
         system('pause')
         system('cls')                
     while True:            
-<<<<<<< HEAD
                 imprimir=(input('Digite I para imprimir a fita de caixa ou o nome do cliente para imprimir específico e zero para fechar pagamento:  ')) 
                 if imprimir=='0':
                     break
@@ -126,27 +125,26 @@ def pagamento():
                     print(list_cliente[len(elemento)])
                     print(list_atraso[i])
                     print(list_valor_final[i])
-                    
-=======
-            imprimir=(input('Digite I para imprimir a fita de caixa ou o nome do cliente para imprimir específico e zero para fechar pagamento:  ')) 
-            if imprimir=='0':
-                break
-            if imprimir =='I' or imprimir=='i':
-                print(list_cliente)
-                print(list_atraso)
-                print(list_valor_final)
-            else:
-                try:
-                    a=list_cliente.index(imprimir)
-                    print(a)
-                    print(list_cliente[a])
-                    print(list_atraso[a])
-                    print(list_valor_final[a])
-                except ValueError:
-                    print('cliente não encontrado')
-                except:
-                    print('cliente inválido!')
-                            
+    while True:                  
+        imprimir=(input('Digite I para imprimir a fita de caixa ou o nome do cliente para imprimir específico e zero para fechar pagamento:  ')) 
+        if imprimir=='0':
+            break
+        if imprimir =='I' or imprimir=='i':
+            print(list_cliente)
+            print(list_atraso)
+            print(list_valor_final)
+        else:
+            try:
+                a=list_cliente.index(imprimir)
+                print(a)
+                print(list_cliente[a])
+                print(list_atraso[a])
+                print(list_valor_final[a])
+            except ValueError:
+                print('cliente não encontrado')
+            except:
+                print('cliente inválido!')
+                        
 
                 
               
